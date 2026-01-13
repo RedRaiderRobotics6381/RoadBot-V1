a0341eed553726e886522b78e299f81b08380119
anitiw <27tiwari372@sasd.net>
2026-01-12 20:52:58 -0600

    Parabolic AutoShooter

--- src/main/java/frc/robot/Commands/Autoshooter.java
+++ src/main/java/frc/robot/Commands/Autoshooter.java
@@ -1,59 +1,70 @@
-package frc.robot.Commands;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.PhysicalConstants;
+import frc.robot.Constants.*;
 import frc.robot.subsystems.Secondary.Indexer;
 import frc.robot.subsystems.Secondary.Outtake;
 import frc.robot.subsystems.Secondary.RotateSubsystem;
 import frc.robot.subsystems.Secondary.Turret;
 import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
 
-public class Autoshooter extends Command {
+public class AutoShooter extends Command {
+    private Indexer m_indexer;
+    private RotateSubsystem m_rotate;
+    private Outtake m_outtake;
+    private Turret m_turret;
+    private CommandSwerveDrivetrain drivetrain;
+    private double distance;
+    private double lowerBoundAngle;
+    private double upperBoundAngle;
+    private double angle;
+    private double yaw;
 
-
-    private final Indexer indexer;
-    private final RotateSubsystem rotateSubsystem;
-    private final Outtake outtake;
-    private final CommandSwerveDrivetrain drivetrain;
-    private final Turret turret;
-
-
-    public double rotateAngle = 0;
-    public double yaw = 0;
-    public int velocity = 0;
-
-    public double distance;
-
-    public Autoshooter(Indexer indexer, RotateSubsystem rotateSubsystem, Outtake outtake, CommandSwerveDrivetrain drivetrain, Turret turret) {
-
+    public AutoShooter(Outtake m_outtake, RotateSubsystem m_rotate, Indexer m_indexer, CommandSwerveDrivetrain drivetrain, Turret m_turret) {
+        this.m_indexer = m_indexer;
+        this.m_outtake = m_outtake;
+        this.m_rotate = m_rotate;
         this.drivetrain = drivetrain;
-        this.rotateSubsystem = rotateSubsystem;
-        this.indexer = indexer;
-        this.outtake = outtake;
-        this.turret = turret;
-
-    }
-    @Override
-    public void initialize() {
-        outtake.setVelocity(velocity);
+        this.m_turret = m_turret;
     }
 
+    @Override
+    public void initialize(){
+    
+    m_outtake.setVelocity(ConstantValues.SHOOTER_RPM);                    
+
+    }
 
     @Override
-    public void execute() {
-        distance = Math.sqrt(Math.pow(182.1 - drivetrain.getState().Pose.getX(), 2) + Math.pow(158.5 - drivetrain.getState().Pose.getY(), 2));
-        rotateAngle = Math.atan((72 - PhysicalConstants.SHOOTER_HEIGHT) / distance);
-        yaw = Math.atan((158.5 - drivetrain.getState().Pose.getY()) / (182.1 - drivetrain.getState().Pose.getX()));
-        turret.setTurretCmd(yaw);
-        rotateSubsystem.setRotateAngleCmd(rotateAngle);
-        if (Math.abs(rotateSubsystem.armAngEnc.getPosition() - rotateAngle) <= .5 && Math.abs(outtake.wheelSpeedEnc.getVelocity() - velocity) <=30) {
-            indexer.indexMtrLdr.set(1);
+    public void execute(){
+        distance =  Math.sqrt(Math.pow(182.1 - drivetrain.getState().Pose.getX(), 2) + Math.pow(158.5 - drivetrain.getState().Pose.getY(), 2));
+        lowerBoundAngle = Math.atan((Math.pow(ConstantValues.SHOOTER_SPEED, 2) + 
+                          Math.sqrt(Math.pow(ConstantValues.SHOOTER_SPEED,4) - 
+                          192 * (192 * Math.pow(distance - FieldConstants.SMALLEST_RADIUS_OF_HUB, 2) + 
+                          2 * (FieldConstants.HEIGHT_OF_HUB - PhysicalConstants.SHOOTER_HEIGHT) * 
+                          Math.pow(ConstantValues.SHOOTER_SPEED, 2)))) / 
+                          192 * (distance - FieldConstants.SMALLEST_RADIUS_OF_HUB));
+    
+        upperBoundAngle = Math.atan((Math.pow(ConstantValues.SHOOTER_SPEED, 2) + 
+                          Math.sqrt(Math.pow(ConstantValues.SHOOTER_SPEED,4) - 
+                          192 * (192 * Math.pow(distance - FieldConstants.SMALLEST_RADIUS_OF_HOLE, 2) + 
+                          2 * (FieldConstants.HEIGHT_OF_HOLE - PhysicalConstants.SHOOTER_HEIGHT) * 
+                          Math.pow(ConstantValues.SHOOTER_SPEED, 2)))) / 
+                          192 * (distance - FieldConstants.SMALLEST_RADIUS_OF_HOLE));
+    
+        angle = (lowerBoundAngle + upperBoundAngle) / 2;
+        m_rotate.setRotateAngleCmd(angle);
+        
+        yaw = Math.atan((158.5 - drivetrain.getState().Pose.getY())/(182.1 - drivetrain.getState().Pose.getX()));
+        m_turret.setTurretCmd(yaw);
+        if(Math.abs(m_rotate.armAngEnc.getPosition() - angle) <= 1 && Math.abs(m_outtake.wheelSpeedEnc.getVelocity() - ConstantValues.SHOOTER_RPM) <= 30 && Math.abs(m_turret.turretAngEnc.getPosition() - yaw) <= 1){
+            m_indexer.indexMtrLdr.set(1);
         }
     }
 
     @Override
-    public void end(boolean interrupted) {
-        outtake.setVelocity(0);
-        indexer.indexMtrLdr.set(0);
+    public void end(boolean interrupted){
+        m_outtake.setVelocity(0);
+        m_indexer.indexMtrLdr.set(0);
     }
-}
+} 
